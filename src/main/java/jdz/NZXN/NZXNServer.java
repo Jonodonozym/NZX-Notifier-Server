09bd9d0e3e0c11bfb801433cdd63d1323564912e
bakerjaid <jaidencolebaker@gmail.com>
2018-07-28 21:01:57 +1200

    fetch new companies and announcements on boot

--- src/main/java/jdz/NZXN/NZXNServer.java
+++ src/main/java/jdz/NZXN/NZXNServer.java
@@ -1,9 +1,14 @@
 
 package jdz.NZXN;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import jdz.NZXN.service.NZXAnnouncementDataFetcher;
+import jdz.NZXN.service.NZXCompanyDataFetcher;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,4 +16,12 @@ public class NZXNServer {
     public static void main(String[] args) {
         SpringApplication.run(NZXNServer.class, args);
     }
+    
+    @Bean
+	public CommandLineRunner demo(NZXCompanyDataFetcher companyDataFetcher, NZXAnnouncementDataFetcher announcementDataFetcher) {
+    	return (args) -> {
+    		companyDataFetcher.update();
+    		announcementDataFetcher.update();
+		};
+	}
 }
