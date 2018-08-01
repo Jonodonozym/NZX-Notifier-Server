ae7b16f1adbed8d08aa357790003d882776f2ac9
bakerjaid <jaidencolebaker@gmail.com>
2018-08-01 13:40:53 +1200

    Added companies controller

---
+++ src/main/java/jdz/NZXN/controller/CompaniesController.java
@@ -0,0 +1,29 @@
+
+package jdz.NZXN.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jdz.NZXN.entity.company.Company;
+import jdz.NZXN.entity.company.CompanyRepository;
+
+@Controller
+@RequestMapping("/companies")
+public class CompaniesController {
+	@Autowired private CompanyRepository repo;
+	
+	@GetMapping
+	public List<Company> fetchAll() {
+		return repo.findAll();
+	}
+	
+	@GetMapping
+	public List<Company> search(@RequestParam String query){
+		return repo.findByIdStartingWith(query);
+	}
+}
