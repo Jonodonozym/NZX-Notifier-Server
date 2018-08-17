e2971753d900449ea7ee93d3c9185d7465293ddb
bakerjaid <jaidencolebaker@gmail.com>
2018-08-17 15:29:59 +1200

    Fixed companies mapping not registered

--- src/main/java/jdz/NZXN/controller/CompaniesController.java
+++ src/main/java/jdz/NZXN/controller/CompaniesController.java
@@ -4,15 +4,15 @@ package jdz.NZXN.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import jdz.NZXN.entity.company.Company;
 import jdz.NZXN.entity.company.CompanyRepository;
 
-@Controller
+@RestController
 @RequestMapping("/companies")
 public class CompaniesController {
 	@Autowired private CompanyRepository repo;
