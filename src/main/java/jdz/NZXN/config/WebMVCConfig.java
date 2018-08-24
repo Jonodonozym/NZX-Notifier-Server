33000b4ce050ff9efd8b16e88897299eb60f2ac1
bakerjaid <jaidencolebaker@gmail.com>
2018-08-24 13:10:43 +1200

    enabled global CORS

--- src/main/java/jdz/NZXN/config/WebMVCConfig.java
+++ src/main/java/jdz/NZXN/config/WebMVCConfig.java
@@ -38,6 +38,6 @@ public class WebMVCConfig extends WebMvcConfigurationSupport {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
 	}
 }
